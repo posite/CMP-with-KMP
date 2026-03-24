12a2873e40346dc2d2ff79a44a08b5760e4fde43
posite <su84053@gmail.com>
2026-03-25 00:54:25 +0900

    chore: fixed untracked files

--- composeApp/src/commonMain/kotlin/com/example/simple/data/MealService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.simple.data
-
-import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import org.jetbrains.compose.resources.getString
-
-class MealService(private val client: HttpClient) {
-    suspend fun getMeals() = client.get(getString(R.string.base_url))
-}
